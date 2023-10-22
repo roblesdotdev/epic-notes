4a25f549c0f8d4a0710684525ab0bd3e01995687
roblesdotdev <httpsnip@gmail.com>
2023-10-22 03:18:44 -0300

    chore: tailwind plugin helper

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended',
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
   ],
-};
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,19 +9,23 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex flex-col justify-center px-4 py-12 gap-2 max-w-2xl mx-auto">
-      <h1 className="flex flex-col font-bold text-3xl">
-        <span>All yuor notes.</span>
-        <span>Organized.</span> <span>Effortless.</span>
-      </h1>
-      <p>
-        Inspiration strikes anywhere.{' '}
-        <span className="font-medium">Epic notes</span> lets you capture,
-        organize and share your ideas accross any device.
-      </p>
-      <button className="px-6 py-3 bg-black text-white self-start font-medium mt-2">
-        Get Started
-      </button>
+    <div className="px-6 pt-10 lg:px-8">
+      <div className="mx-auto max-w-2xl py-32 sm:py-48">
+        <h1 className="flex flex-col text-4xl font-bold sm:text-6xl">
+          <span>All yuor notes.</span>
+          <span>Organized.</span> <span>Effortless.</span>
+        </h1>
+        <p className="mt-4 leading-6 text-gray-700 sm:text-lg">
+          Inspiration strikes anywhere.{' '}
+          <span className="font-medium">Epic notes</span> lets you capture,
+          organize and share your ideas accross any device.
+        </p>
+        <div className="mt-2">
+          <button className="mt-2 self-start rounded-lg bg-black px-6 py-3 font-medium text-white">
+            Get Started
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
