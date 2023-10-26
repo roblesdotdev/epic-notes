794568d1a4d34c7558aac67e4ad1f497252a30b8
roblesdotdev <httpsnip@gmail.com>
2023-10-26 11:34:18 -0300

    upgrade/setup express server

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -11,6 +11,10 @@ import { createReadableStreamFromReadable } from '@remix-run/node'
 import { RemixServer } from '@remix-run/react'
 import isbot from 'isbot'
 import { renderToPipeableStream } from 'react-dom/server'
+import { getEnv, init } from './utils/env.server.ts'
+
+init()
+global.ENV = getEnv()
 
 const ABORT_DELAY = 5_000
 
