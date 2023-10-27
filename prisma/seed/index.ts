5d75149dbc3bbf7c973968e07ebf1776818716ff
roblesdotdev <httpsnip@gmail.com>
2023-10-27 09:39:57 -0300

    data-modeling/seed script

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -3,32 +3,53 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const firstNote = await prisma.note.findFirst()
+async function seed() {
+  // clean database
+  await prisma.user.deleteMany()
 
-if (!firstNote) {
-  throw new Error('You need to have a note in the database first')
+  // Insert base user with notes
+  await prisma.user.create({
+    data: {
+      email: 'kody@email.com',
+      username: 'kody',
+      name: 'Kody User',
+      notes: {
+        create: [
+          {
+            id: 'd27a197e',
+            title: 'Basic koala facts',
+            content:
+              'Koalas are found in the eucalyptus forests of eastern Australia. They have grey fur with a cream-coloured chest, and strong, clawed feet, perfect for living in the branches of trees!',
+            images: {
+              create: [
+                {
+                  altText: 'an adorable koala cartoon illustration',
+                  contentType: 'image/png',
+                  blob: await fs.promises.readFile(
+                    './tests/fixtures/images/kody-notes/cute-koala.png',
+                  ),
+                },
+                {
+                  altText: 'a cartoon illustration of a koala in a tree eating',
+                  contentType: 'image/png',
+                  blob: await fs.promises.readFile(
+                    './tests/fixtures/images/kody-notes/koala-eating.png',
+                  ),
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  })
 }
 
-await prisma.note.update({
-  where: { id: firstNote.id },
-  data: {
-    images: {
-      create: [
-        {
-          altText: 'an adorable koala cartoon illustration',
-          contentType: 'image/png',
-          blob: await fs.promises.readFile(
-            './tests/fixtures/images/kody-notes/cute-koala.png',
-          ),
-        },
-        {
-          altText: 'a cartoon illustration of a koala in a tree eating',
-          contentType: 'image/png',
-          blob: await fs.promises.readFile(
-            './tests/fixtures/images/kody-notes/koala-eating.png',
-          ),
-        },
-      ],
-    },
-  },
-})
+seed()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
